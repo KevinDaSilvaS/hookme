316f10ca9c423c7bdd05a44fc39a3ca2bfb0d6a0
Kevin <kevin.kds80@gmail.com>
2023-08-04 22:23:54 +0000

    start file

---
+++ lib/hookme.ex
@@ -0,0 +1,18 @@
+defmodule Hookme do
+  @moduledoc """
+  Documentation for `Hookme`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hookme.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
