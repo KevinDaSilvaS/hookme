dd0e0ed01355d670f6bd61564100c36938de72d1
Kevin <kevin.kds80@gmail.com>
2023-08-04 22:24:25 +0000

    add validators for entrypoint inputs

---
+++ lib/app/validators/entrypoint_validators.ex
@@ -0,0 +1,9 @@
+defmodule App.Validators.EntrypointValidators do
+
+  def sanitize_input(input), do: String.replace(input, "\n", "")
+
+  def validate_proceed("Y"), do: true
+  def validate_proceed("y"), do: true
+  def validate_proceed(""), do: true
+  def validate_proceed(_), do: false
+end
